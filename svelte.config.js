b05e9c64715a624568ac856ab72b0c26c656eba8
Eric Liu <ericyl.us@gmail.com>
2022-01-30 17:38:29 -0800

    build: remove appDir

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
-export default {
+const config = {
   kit: {
     adapter: adapter(),
     target: "#svelte",
-    appDir: "app",
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages/" : "",
+      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
     },
   },
 };
+
+export default config;
