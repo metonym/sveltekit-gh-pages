08e655d56b34d8f170874ed1aa0b550efcbf51d7
Eric Liu <ericyl.us@gmail.com>
2022-01-30 17:38:40 -0800

    chore: use trailing slash in link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <h1>sveltekit-gh-pages</h1>
+<p>Deployed to GitHub Pages</p>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<a href="about">About</a>
+<a href="about/">About</a>
