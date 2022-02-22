266f81792b9bf72116ff4a58849c1ed016344c71
metonym <ericyl.us@gmail.com>
2022-02-22 06:56:31 -0800

    fix: prefix hrefs with `base`

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,10 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <h1>sveltekit-gh-pages</h1>
 <p>Deployed to GitHub Pages</p>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
-<a href="about/">About</a>
+ <a href="{base}/about">About</a>
