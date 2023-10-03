682cede7ed0b5a5748957b419cc182ae71f7ffab
metonym <ericyl.us@gmail.com>
2023-10-03 10:47:52 -0700

    Updates

---
+++ _app/immutable/chunks/paths.ee7f12fe.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1sy4au3)==null?void 0:s.base)??"/sveltekit-gh-pages";var a;const t=((a=globalThis.__sveltekit_1sy4au3)==null?void 0:a.assets)??e;export{t as a,e as b};
