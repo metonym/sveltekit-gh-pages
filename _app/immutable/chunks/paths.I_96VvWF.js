e49417902301fbb9afc5e515179410ff96d4e569
Eric Liu <ericyl.us@gmail.com>
2023-12-24 09:41:03 -0800

    Updates

--- _app/immutable/chunks/paths.I_96VvWF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_f8q53v?.base??"/sveltekit-gh-pages",e=globalThis.__sveltekit_f8q53v?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/paths.KRkwhBTe.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_cqi3yp?.base??"",a=globalThis.__sveltekit_cqi3yp?.assets??s;export{a,s as b};
