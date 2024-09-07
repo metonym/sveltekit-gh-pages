ac32e8c6177739b912472c9e5930c4dfdce83ee7
metonym <ericyl.us@gmail.com>
2024-09-07 06:19:03 -0600

    chore: reword about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
 <h1>About</h1>
 <p>Reload this page. You should see the same content.</p>
-<p>Inspect the page source; the HTML should be pre-rendered.</p>
+<p>View the page source: the HTML is pre-rendered.</p>
